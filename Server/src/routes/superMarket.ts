1d8edb87de29bc8d205227980d3e0d89bf971a9b
kibudi <shahar002000@gmail.com>
2025-08-06 11:22:19 +0300

    added delete Supermarket

--- Server/src/routes/superMarket.ts
+++ Server/src/routes/superMarket.ts
@@ -1,10 +1,19 @@
 import express from "express";
-import { authenticateToken } from "../middleware/auth";
+import { authenticateToken, checkIsAdmin } from "../middleware/auth";
 import SuperMarketController from "../controllers/SuperMarketControllers";
 
 const router = express.Router();
 
-router.post("/create", authenticateToken, SuperMarketController.createStore);
-
-
+router.post(
+  "/create",
+  authenticateToken,
+  checkIsAdmin,
+  SuperMarketController.createStore
+);
+router.delete(
+  "/delete/:storeId",
+  authenticateToken,
+  checkIsAdmin,
+  SuperMarketController.deleteStore
+);
 export default router;
