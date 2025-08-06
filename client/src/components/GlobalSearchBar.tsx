573484a96e82042b35fdf68ae641a4d185e4e8d5
Yuval Amit <yuval@Yuvals-MacBook-Pro.local>
2025-08-06 21:52:46 +0300

    fix

--- client/src/components/GlobalSearchBar.tsx
+++ client/src/components/GlobalSearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Search, X } from "lucide-react";
 import { useSearch } from "../contexts/SearchContext";
 import { useFetchProducts } from "../hooks/useFetch";
