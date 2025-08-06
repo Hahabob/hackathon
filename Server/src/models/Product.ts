c8f8875102d877730ea601dbb877e730d6e3ef00
hahabob <orisor101@gmail.com>
2025-08-06 10:07:43 +0300

    created product  schema

--- Server/src/models/Product.ts
+++ Server/src/models/Product.ts
@@ -1,3 +1,49 @@
 import { Document, Schema, Types, model } from "mongoose";
 
-interface IProduct extends Document {}
+interface IProduct extends Document {
+  _id: Types.ObjectId;
+  name: string;
+  producer: string;
+  category: string;
+  barcode: string;
+  location?: {
+    x: number;
+    y: number;
+  };
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const productSchema = new Schema<IProduct>(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    producer: {
+      type: String,
+      require: true,
+    },
+    category: {
+      type: String,
+      require: true,
+    },
+    location: {
+      x: {
+        type: Number,
+      },
+      y: {
+        type: Number,
+      },
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Product = model<IProduct>("Product", productSchema);
+
+export default Product;
