5981c3b9b868b0e80251163faeb6a8b7bcefe868
hahabob <orisor101@gmail.com>
2025-08-06 13:14:35 +0300

    ssf

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
 
-import { CartProvider } from "./contexts/CartContext";
+import CartProvider from "./contexts/CartProvider";
 
 const queryClient = new QueryClient();
 
