57ac9f4780c71427fa5593aa2cd4e7aa586f1156
hahabob <orisor101@gmail.com>
2025-08-06 13:30:25 +0300

    added baseurl api from axios

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -2,3 +2,9 @@
 // It will be used by various components and contexts to fetch or send data.
 // Best practice: Use a library like Axios or the native Fetch API with a consistent error handling mechanism.
 // Abstract away the base URL and authentication headers.
+
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "http://localhost:3000/api",
+});
