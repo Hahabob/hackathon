1c6542b8ea2dfbeb76597ff14d055a272e58fabd
hahabob <orisor101@gmail.com>
2025-08-06 14:01:28 +0300

    added shadcn and zod

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,22 @@
 // src/App.tsx
-import { Routes, Route } from "react-router-dom";
-import ProductCatalogPage from "./pages/ProductCatalogPage";
-import ShoppingCartPage from "./pages/ShoppingCartPage";
-import MapPage from "./pages/MapPage";
+// import { Routes, Route } from "react-router-dom";
+// import ProductCatalogPage from "./pages/ProductCatalogPage";
+// import ShoppingCartPage from "./pages/ShoppingCartPage";
+// import MapPage from "./pages/MapPage";
 
-import Header from "./components/Header";
-import Navigation from "./components/Navigation";
+import Sidebar from "./components/SideBar";
+import { useAuth } from "./contexts/AuthContext";
+import { useSidebar } from "./contexts/SideBarContext";
+
+// import Header from "./components/Header";
+// import Navigation from "./components/Navigation";
 
 const App = () => {
+  const { toggleSidebar } = useSidebar();
+  const { user } = useAuth();
   return (
     <>
-      <Header />
+      {/* <Header />
       <Navigation />
       <main className="px-4 py-6">
         <Routes>
@@ -18,7 +24,14 @@ const App = () => {
           <Route path="/cart" element={<ShoppingCartPage />} />
           <Route path="/map" element={<MapPage />} />
         </Routes>
-      </main>
+      </main> */}
+      <button
+        onClick={toggleSidebar}
+        className="px-4 py-2 bg-blue-600 text-white rounded"
+      >
+        {user ? user.name?.charAt(0) : `sidebar`}
+      </button>
+      <Sidebar />
     </>
   );
 };
